d2bd6192800ff958153cd337d0910f6ceba476d8
Les Harris <les@lesharris.com>
2017-10-15 20:22:37 -0700

    Update meta.js

--- meta.js
+++ meta.js
@@ -1,31 +1,29 @@
 module.exports = {
-  "helpers": {
-    "if_or": function (v1, v2, options) {
+  'helpers':         {
+    'if_or': function (v1, v2, options) {
       if (v1 || v2) {
-        return options.fn(this);
+        return options.fn(this)
       }
 
-      return options.inverse(this);
+      return options.inverse(this)
     }
   },
-  "prompts": {
-    "name": {
-      "type": "string",
-      "required": true,
-      "message": "Project name"
+  'prompts':         {
+    'name':        {
+      'type':     'string',
+      'required': true,
+      'message':  'Project name'
     },
-    "description": {
-      "type": "string",
-      "required": false,
-      "message": "Project description",
-      "default": "A Vue.js project"
+    'description': {
+      'type':     'string',
+      'required': false,
+      'message':  'Project description',
+      'default':  'A Vue.js project'
     },
-    "author": {
-      "type": "string",
-      "message": "Author"
-    },
-  "filters": {
-    "src/router/**/*": "router"
+    'author':      {
+      'type':    'string',
+      'message': 'Author'
+    }
   },
-  "completeMessage": "To get started:\n\n  {{^inPlace}}cd {{destDirName}}\n  {{/inPlace}}yarn install\n  yarn run build"
-};
+  'completeMessage': 'To get started:\n\n  {{^inPlace}}cd {{destDirName}}\n  {{/inPlace}}yarn install\n  yarn run build'
+}
